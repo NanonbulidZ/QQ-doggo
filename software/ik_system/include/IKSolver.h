68e3789cf45662e17e7ccdf9f4eaa53f7930650b
Natthaphat Charoenchaikorn <nanonkrub@gmail.com>
2026-02-13 20:51:39 +0700

    Add IKSolver.h and related IK system header files

---
+++ software/ik_system/include/IKSolver.h
@@ -0,0 +1,12 @@
+// Main inverse kinematics solver with LegJoints struct and LegIK class
+
+// Definition here
+
+struct LegJoints {
+    // Joint angles
+};
+
+class LegIK {
+public:
+    // IK calculation methods
+};
